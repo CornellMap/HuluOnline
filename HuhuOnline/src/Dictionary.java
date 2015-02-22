5c78ce67fa40239d91b8e46e15eaa48751a68ee7
SherryXia <mx76@cornell.edu>
2015-02-22 13:47:45 -0500

    changes

--- HuhuOnline/src/Dictionary.java
+++ HuhuOnline/src/Dictionary.java
@@ -52,6 +52,6 @@ public class Dictionary {
 	
 	
 	public static void main(String[] args) {
-		System.out.println(new Dictionary().wordList.size());
+		System.out.println(new Dictionary().wordList.contains("fifteenth"));
 	}
 }
--- HuhuOnline/src/Guess.java
+++ HuhuOnline/src/Guess.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,6 +14,17 @@ public class Guess {
 	Prison prison = null;
 	// Frequency position;
 	int freqPosition = 0;
+	
+	// Common bi-letter words
+	String[] commonBiLetterWords = new String[]{
+	    "an", "as", "at", "am", "be", "by", "do",
+		"go", "he", "hi", "is", "it", "if", "in",
+		"my", "no", "of", "on", "or", "oh", 
+		"so", "to", "up", "us", "we,"
+	};
+	
+	// and & the
+	String[] three = new String[]{"and", "the"};
 	
 	String nowTmpToken = "";
 	int success = 0, fail = 0;
@@ -43,55 +53,175 @@ public class Guess {
 		return Dictionary.frequency[freqPosition];
 	}
 	
+	public Character twoGuesser(String pattern) {
+		int count = 0;
+		if (pattern.charAt(0) == '_') count++;
+		if (pattern.charAt(1) == '_') count++;
+		if (count != 1) return null;
+		for (int i = 0; i < commonBiLetterWords.length; i++) {
+			String word = commonBiLetterWords[i];
+			if (pattern.charAt(0) == '_' && !guessed.contains(word.charAt(0))) return word.charAt(0);
+			if (pattern.charAt(1) == '_' && !guessed.contains(word.charAt(1))) return word.charAt(1);
+		}
+		return null;
+	}
+	
+	public Character threeGuesser(String pattern) {
+		int count = 0;
+		if (pattern.charAt(0) == '_') count++;
+		if (pattern.charAt(1) == '_') count++;
+		if (pattern.charAt(2) == '_') count++;
+		
+		if (count != 1) return null;
+		for (int i = 0; i < three.length; i++) {
+			String word = three[i];
+			if (pattern.charAt(0) == '_' && !guessed.contains(word.charAt(0))) return word.charAt(0);
+			if (pattern.charAt(1) == '_' && !guessed.contains(word.charAt(1))) return word.charAt(1);
+			if (pattern.charAt(2) == '_' && !guessed.contains(word.charAt(2))) return word.charAt(2);
+		}
+		return null;
+	}
+	
 	public void doGuess() {
-		if (guessed.size() < 3) {
-			guessChar(nextFrequency());
-		} else {
-			ArrayList<ArrayList<String>> guessPattern = new ArrayList<ArrayList<String>>();
-			int minPossible = 1 << 20;
-			for (String pattern : prison.state) {
-				ArrayList<String> possible = getPossible(pattern);
-				guessPattern.add(possible);
-				minPossible = Math.min(minPossible, possible.size() > 0 ? possible.size() : minPossible);
+		boolean twoguesser = false;
+		for (int i = 0; i < prison.state.size(); i++) {
+			String pattern = prison.state.get(i);
+			if (pattern.length() == 2) {
+				Character ch = twoGuesser(pattern);
+				if (ch != null) {
+					twoguesser = true;
+					guessChar(ch.charValue());
+				}
 			}
-			if (minPossible > 10) {
-				// Too ambiguious to be determined
+		}
+
+		boolean threeguesser = false;
+		if (!twoguesser) {
+			for (int i = 0; i < prison.state.size(); i++) {
+				String pattern = prison.state.get(i);
+				if (pattern.length() == 3) {
+					Character ch = threeGuesser(pattern);
+					if (ch != null) {
+						threeguesser = true;
+						guessChar(ch.charValue());
+					}
+				}
+			}
+		}
+
+		if (!twoguesser && !threeguesser) {
+			if (guessed.size() < 3) {
 				guessChar(nextFrequency());
-			} else if (minPossible == 1){
-				// Determined by majority occurence of solo possible
-				int[] occurence = new int[26];
-				for (int i = 0; i < 26; i++) occurence[i] = 0;
-				
-				for (int i = 0; i < guessPattern.size(); i++) {
-					if (guessPattern.get(i).size() == 1) {
-						String word = guessPattern.get(i).get(0);
-						for (int j = 0; j < word.length(); j++) {
-							if (word.charAt(j) != '_') {
-								occurence[word.charAt(j) - 'a']++;
+			} else {
+				ArrayList<ArrayList<String>> guessPattern = new ArrayList<ArrayList<String>>();
+				int minPossible = 1 << 20;
+				for (String pattern : prison.state) {
+					ArrayList<String> possible = getPossible(pattern);
+					guessPattern.add(possible);
+					minPossible = Math
+							.min(minPossible,
+									possible.size() > 0 ? possible.size()
+											: minPossible);
+				}
+				/*if (minPossible > 10) {
+					// Too ambiguious to be determined
+					int[] occurence = new int[26];
+					for (int i = 0; i < 26; i++)
+						occurence[i] = 0;
+
+					for (int i = 0; i < guessPattern.size(); i++) {
+						for (int k = 0; k < guessPattern.get(i).size(); k++) {
+							String word = guessPattern.get(i).get(k);
+							for (int j = 0; j < word.length(); j++) {
+								if (word.charAt(j) != '_') {
+									occurence[word.charAt(j) - 'a']++;
+								}
 							}
 						}
 					}
-				}
-				
-				int maxOccur = 0;
-				char c = '$';
-				for (int i = 0; i < 26; i++) {
-					if (occurence[i] > maxOccur) {
-						maxOccur = occurence[i];
-						c = (char)(i + 'a');
+
+					double maxOccur = 0;
+					char c = '$';
+					for (int i = 0; i < 26; i++) {
+						if (occurence[i] * Dictionary.frequencyNumber[i] > maxOccur) {
+							maxOccur = occurence[i]
+									* Dictionary.frequencyNumber[i];
+							c = (char) (i + 'a');
+						}
+					}
+					if (c != '$') {
+						guessChar(c);
+					} else {
+						guessChar(nextFrequency());
+					}
+				} else */
+				if (minPossible == 1) {
+					// Determined by majority occurence of solo possible
+					int[] occurence = new int[26];
+					for (int i = 0; i < 26; i++)
+						occurence[i] = 0;
+
+					for (int i = 0; i < guessPattern.size(); i++) {
+						if (guessPattern.get(i).size() == 1) {
+							String word = guessPattern.get(i).get(0);
+							for (int j = 0; j < word.length(); j++) {
+								if (word.charAt(j) != '_') {
+									occurence[word.charAt(j) - 'a']++;
+								}
+							}
+						}
+					}
+
+					int maxOccur = 0;
+					char c = '$';
+					for (int i = 0; i < 26; i++) {
+						if (occurence[i] > maxOccur) {
+							maxOccur = occurence[i];
+							c = (char) (i + 'a');
+						}
+					}
+					if (c != '$') {
+						guessChar(c);
+					} else {
+						guessChar(nextFrequency());
+					}
+				} else {
+					// majority vote, then scale by letter frequency
+					int[] occurence = new int[26];
+					for (int i = 0; i < 26; i++)
+						occurence[i] = 0;
+
+					for (int i = 0; i < guessPattern.size(); i++) {
+						//if (guessPattern.get(i).size() < 10) {
+							for (int k = 0; k < guessPattern.get(i).size(); k++) {
+								String word = guessPattern.get(i).get(k);
+								for (int j = 0; j < word.length(); j++) {
+									if (word.charAt(j) != '_') {
+										occurence[word.charAt(j) - 'a']++;
+									}
+								}
+							}
+						//}
+					}
+
+					double maxOccur = 0;
+					char c = '$';
+					for (int i = 0; i < 26; i++) {
+						if (occurence[i] * Dictionary.frequencyNumber[i] > maxOccur) {
+							maxOccur = occurence[i]
+									* Dictionary.frequencyNumber[i];
+							c = (char) (i + 'a');
+						}
+					}
+					if (c != '$') {
+						guessChar(c);
+					} else {
+						guessChar(nextFrequency());
 					}
 				}
-				if (c != '$') {
-					guessChar(c);
-				} else {
-					guessChar(nextFrequency());
-				}
-			} else {
-				// majority vote, then scale by letter frequency
-				
 			}
-			System.out.println(guessPattern);
 		}
+
 		
 		System.out.println("Prison: " + prison.token + " Status: " + prison.status +" Now State: " + prison.state);
 		if (prison.status == "FREE" && nowTmpToken == prison.token) {
@@ -114,10 +244,23 @@ public class Guess {
 		}
 	}
 	
+	private boolean endMatch(String ori, String pattern) {
+		if (pattern.length() > ori.length()) return false;
+		for (int i = 0; i < pattern.length(); i++) {
+			if (ori.charAt(ori.length() - pattern.length() + i) != pattern.charAt(i) &&
+				pattern.charAt(i) != '_') {
+				return false;
+			}
+		}
+		return true;
+	}
 	private ArrayList<String> getPossible(String pattern) {
 		ArrayList<String> result = new ArrayList<String>();
 		for (String word : dict.wordList) {
-			if (word.length() == pattern.length()) {
+			if (word.length() == pattern.length() || 
+				(word.length() == pattern.length() - 1 && endMatch(pattern, "s")) ||
+				(word.length() == pattern.length() - 2 && endMatch(pattern, "es"))
+				) {
 				boolean valid = true;
 				for (int i = 0; i < word.length(); i++) {
 					if (pattern.charAt(i) != '_' && pattern.charAt(i) != word.charAt(i)) {
@@ -143,17 +286,17 @@ public class Guess {
 	}
 	
 	public static void main(String[] args) {
-//		Guess guess = new Guess();
-//		//guess.newGuess();
-//		//guess.doGuess();
+		Guess guess = new Guess();
+		//guess.newGuess();
+		//guess.doGuess();
 //		guess.guessed.add('t');
 //		guess.guessed.add('e');
 //		guess.guessed.add('a');
 //		
-//		ArrayList<String> result = guess.getPossible("t_e_");
-//		for (String word : result) {
-//			System.out.println(word);
-//		}
-//		
+		ArrayList<String> result = guess.getPossible("_o_got");
+		for (String word : result) {
+			System.out.println(word);
+		}
+		
 	}
 }

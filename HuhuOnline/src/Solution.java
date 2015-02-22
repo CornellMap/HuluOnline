dea6eb78a1fd9d57a1dd3bb06f4d620682d11b92
SherryXia <mx76@cornell.edu>
2015-02-22 11:04:48 -0500

    main

---
+++ HuhuOnline/src/Solution.java
@@ -0,0 +1,28 @@
+import org.json.JSONObject;
+public class Solution {
+    public Solution(){
+    	
+    	Guess guess = new Guess();
+    	guess.newGuess();
+    	while (true){
+    		if (guess.prison.status.equals("ALIVE")) {
+    			guess.doGuess();
+    		} else {
+    			guess.newGuess();
+    		}
+    		
+    		try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    }
+
+    public static void main(String[] args) {
+		Solution solution = new Solution();
+	//	System.out.println(solution);
+	}
+}
+
